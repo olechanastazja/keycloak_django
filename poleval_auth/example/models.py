d98addf73a9848c99aae50fbc3efc18640220dfe
beata <you@example.com>
2020-12-20 18:39:03 +0100

    Remove old version

--- poleval_auth/example/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    keycloack_id = models.CharField(max_length=100)
-
--- poleval_auth/example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.hello_world),
-    path('user_exists/', views.user_exists),
-    path('create_user/', views.create_user),
-]
